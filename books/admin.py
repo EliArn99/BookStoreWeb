c4ca20fcf85059f5a43e862208026dad5fa39e88
EliArn99 <142402112+EliArn99@users.noreply.github.com>
2024-11-26 14:37:38 +0200

    Update admin.py

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Customer, Product, Order, OrderItem, ShippingAddress
 
-# Register your models here.
+from django.contrib import admin
+from .models import BlogPost
+
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
+
+admin.site.register(BlogPost)
