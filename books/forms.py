32359b6c2a530ed36fc5af9b8418cb3daee247f3
EliArn99 <142402112+EliArn99@users.noreply.github.com>
2024-11-27 16:21:20 +0200

    Create forms.py

---
+++ books/forms.py
@@ -0,0 +1,64 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+from .models import BlogPost, Order, Product
+from .models import Customer
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+        help_texts = {
+            'username': None,  # Remove help text for username
+        }
+        error_messages = {
+            'password1': {
+                'required': 'Please enter a strong password.',
+            },
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name in self.fields:
+            self.fields[field_name].help_text = None
+
+
+class BlogPostForm(forms.ModelForm):
+    class Meta:
+        model = BlogPost
+        fields = ['title', 'content']
+
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ['customer', 'complete', 'transaction_id']
+        widgets = {
+            'transaction_id': forms.TextInput(attrs={'placeholder': 'Enter Transaction ID'}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['customer'].queryset = Customer.objects.all()
+
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['name', 'price', 'digital', 'image']
+        widgets = {
+            'price': forms.NumberInput(attrs={'min': 0, 'step': '0.01'}),
+        }
+
+
+class CustomerForm(forms.ModelForm):
+    class Meta:
+        model = Customer
+        fields = ['name', 'email']
+        widgets = {
+            'name': forms.TextInput(attrs={'placeholder': 'Enter Full Name'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'Enter Email Address'}),
+        }
+
+
