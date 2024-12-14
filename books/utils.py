a58afaf8b8f937a09de8f54df9b21914124095d6
EliArn99 <142402112+EliArn99@users.noreply.github.com>
2024-12-14 15:23:49 +0200

    Update utils.py

--- books/utils.py
+++ books/utils.py
@@ -15,7 +15,6 @@ def cookieCart(request):
     cartItems = order['get_cart_items']
 
     for i in cart:
-        # We use try block to prevent items in cart that may have been removed from causing error
         try:
             cartItems += cart[i]['quantity']
 
