51621536c8965e0a66d73e892af70d745c006276
EliArn99 <142402112+EliArn99@users.noreply.github.com>
2024-12-14 15:23:12 +0200

    Update settings.py

--- BookStoreWeb/settings.py
+++ BookStoreWeb/settings.py
@@ -71,11 +71,19 @@ TEMPLATES = [
 ]
 
 
+# REST_FRAMEWORK = {
+#     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+#     'PAGE_SIZE': 10,
+# }
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
 }
-
 WSGI_APPLICATION = 'BookWeb_demo.wsgi.application'
 
 
