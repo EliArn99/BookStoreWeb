172fc9763da9c416ee931012a8175c1b4db38d03
Eli Arnaytska <142402112+EliArn99@users.noreply.github.com>
2025-08-29 17:36:30 +0300

    Update admin.py

--- books/admin.py
+++ books/admin.py
@@ -1,48 +1,91 @@
 from django.contrib import admin
-from .models import Product, Order, Customer, OrderItem, ShippingAddress, BlogPost
+from django.utils.safestring import mark_safe
+from .models import Customer, Book, Order, OrderItem, ShippingAddress, Category, Review, Post, Banner
 
 
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'digital')
-    list_filter = ('digital',)
-    search_fields = ('name',)
-    ordering = ('price',)
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author', 'price', 'stock', 'category', 'is_digital', 'image_tag')
+
+    list_editable = ('price', 'stock')
+
+    list_filter = ('category', 'digital', 'stock')
+    search_fields = ('name', 'author', 'description')
+
+    def image_tag(self, obj):
+        if obj.image:
+            return mark_safe('<img src="%s" width="50" height="75" style="object-fit: cover;" />' % obj.image.url)
+        return "Няма снимка"
+
+    image_tag.short_description = 'Снимка'
+
+    def is_digital(self, obj):
+        return "Да" if obj.digital else "Не"
+
+    is_digital.short_description = 'Дигитална'
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ['product']
+    extra = 0
+    readonly_fields = ['product_name', 'product_price']
+
+    def product_name(self, obj):
+        return obj.product.name
+
+    product_name.short_description = 'Продукт'
+
+    def product_price(self, obj):
+        return f"{obj.product.price} лв."
+
+    product_price.short_description = 'Цена'
 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('id', 'customer', 'complete', 'transaction_id', 'date_ordered')
+    list_display = ('id', 'customer', 'date_ordered', 'complete', 'get_cart_total', 'transaction_id')
     list_filter = ('complete', 'date_ordered')
-    search_fields = ('transaction_id',)
-    ordering = ('-date_ordered',)
+    search_fields = ('customer__name', 'transaction_id')
+    inlines = [OrderItemInline]
+    readonly_fields = ('get_cart_total',)
+
+    def get_cart_total(self, obj):
+        return f"{obj.get_cart_total} лв."
+
+    get_cart_total.short_description = 'Обща сума'
 
 
-@admin.register(Customer)
-class CustomerAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'user')
-    search_fields = ('name', 'email')
-    ordering = ('name',)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ('name', 'slug')
+    search_fields = ('name',)
 
 
-@admin.register(OrderItem)
-class OrderItemAdmin(admin.ModelAdmin):
-    list_display = ('product', 'order', 'quantity', 'date_added')
-    list_filter = ('date_added',)
-    ordering = ('-date_added',)
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('book', 'user', 'rating', 'created_at', 'comment')
+    list_filter = ('rating', 'book')
+    search_fields = ('book__name', 'user__username', 'comment')
+    readonly_fields = ('created_at',)
+    list_per_page = 20
 
 
-@admin.register(ShippingAddress)
-class ShippingAddressAdmin(admin.ModelAdmin):
-    list_display = ('customer', 'order', 'address', 'city', 'state', 'zipcode', 'date_added')
-    list_filter = ('city', 'state')
-    search_fields = ('address', 'city', 'state', 'zipcode')
-    ordering = ('-date_added',)
+admin.site.register(Customer)
+admin.site.register(ShippingAddress)
 
 
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'status', 'created_on')
+    list_filter = ("status",)
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
 
 
-@admin.register(BlogPost)
-class BlogPostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'created_at')
-    fields = ('title', 'content', 'image', 'author')
+@admin.register(Banner)
+class BannerAdmin(admin.ModelAdmin):
+    list_display = ('title', 'is_active', 'order')
+    list_filter = ('is_active',)
+    search_fields = ('title',)
